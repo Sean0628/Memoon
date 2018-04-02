95d83e74fd3a22bad7eb7a1c2c76ef8b70f076ee
Sho Ito <i.sho.0628@gmail.com>
2018-04-02 12:37:26 +0900

    define new method in linebot_controller

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -41,3 +41,19 @@ class LinebotController < ApplicationController
     }
   end
 end
+
+private
+  def save_memo(input)
+    # if it includes a title.
+    if input.start_with?('#' || '＃')
+      # gets the title.
+      title = input.split("\n")[0]
+      # gets body.
+      body = input.split("\n").drop(1).join("\n")
+    # if there is not a title
+    else
+      title = "##{input[0..10]}"
+      body = input
+    end
+  end
+
