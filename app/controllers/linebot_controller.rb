94de0b678b758860acb83d5f12afeb0d3ab4b951
Sho Ito <i.sho.0628@gmail.com>
2018-04-02 23:41:06 +0900

    modify fatal bugs

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -34,7 +34,8 @@ class LinebotController < ApplicationController
           when input == 'list' || 'リスト' || 'りすと'
             @memos = @user.memos.limit(5)
             # TO DO: insert message generator
-            client.reply_message(event['replyToken'], generate_message(@memos))
+            message = generate_message(@memos)
+            client.reply_message(event['replyToken'], message)
           # generate new memos
           else
             # if it includes a title.
